a8f84352616f1d2ba0e1d0919a42851728f68e3c
Vichai Vongvorakul <vongvichai@gmail.com>
2023-11-24 23:45:46 +0700

    added surname

--- src/App.js
+++ src/App.js
@@ -16,7 +16,7 @@ function App()
           target="_blank"
           rel="noopener noreferrer"
         >
-          Learn React From Vichai
+          Learn React From Vichai Vongvorakul
         </a>
       </header>
     </div>
